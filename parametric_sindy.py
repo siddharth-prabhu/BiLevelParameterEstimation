465f38d3c77e594085b4afc7c49d02aef17d9cb7
scp220 <scp220@lehigh.edu>
2024-07-02 17:47:01 -0400

    added equality constraints for the inner optimization

--- parametric_sindy.py
+++ parametric_sindy.py
@@ -1,5 +1,7 @@
 import os
 from functools import partial
+from typing import List, Tuple
+import operator
 
 import jax
 import jax.numpy as jnp
@@ -75,45 +77,115 @@ def _data_matrix(p, features, T):
     _p = jax.vmap(rate_constant, in_axes = (0, None, None))(T, jnp.array(373.), p)
     return jnp.vstack(jax.vmap(lambda _p, feat : _p * feat)(_p, features))
 
+def equality_constraints(p, theta, nx):
+    # The equality constraints can itself be nonlinear but given p, they should be linear with respect to theta.
+    # flattened inputs. Reshape theta and then specify constraints for simplicity
+    # Right now can only add constraints with respect to p and theta for the terms that are present in the original model
+    # TODO add args i.e. temperature and other variables
+    theta = theta.reshape(nx, -1)
+    return jnp.array([
+        theta[0, 1] + theta[1, 1],
+        theta[0, 4] + theta[1, 4],
+        theta[2, 2] + theta[3, 2]
+    ])
 
 @partial(jax.custom_vjp, nondiff_argnums = (6, 7))
 def _optimal_parameters(p, target, features, temperature, permute, inverse_permute, nonzero_cols, regularization):
     
+    regularization = jnp.maximum(regularization, 1e-12)
+    nx, _ = permute.shape
     A = _data_matrix(p, features, temperature)
     m, n = A.shape
-    A = A[:, permute]
+    theta_guess = jnp.zeros(n * nx)
     
-    theta = jnp.zeros(n)
-    _A = jax.lax.slice(A, (0, 0), (m, nonzero_cols))
-    u, s, vh = jnp.linalg.svd(_A.T @ _A + regularization * jnp.eye(nonzero_cols))
-    _theta = vh.T @ (jnp.diag(1/s) @ (u.T @ (_A.T @ target)))
-    # _theta = jnp.linalg.solve(_A.T @ _A + regularization * jnp.eye(nonzero_cols), _A.T @ target)
-    theta = theta.at[jnp.arange(nonzero_cols, dtype = int)].set(_theta)
+    g = equality_constraints(p, theta_guess, nx)
+    g_x = jax.jacobian(lambda _theta : equality_constraints(p, _theta, nx))(theta_guess) # primals can be zero since constraints are linear
 
-    return theta[inverse_permute], (u, s, vh)
+    def unconstrained(_A, _permute, _inverse_permute, _nonzero_cols, _target):
+        # returns the unconstrained solution and the inverse matrix 
+        theta = jnp.zeros(n)
+        _zero_cols = n - _nonzero_cols
+        _A = jax.lax.slice(_A[:, _permute], (0, 0), (m, _nonzero_cols))
+        _inv = jnp.linalg.pinv(_A.T @ _A + regularization * jnp.eye(_nonzero_cols))
+        _theta = _inv @ (_A.T @ _target)
+        
+        theta = theta.at[jnp.arange(_nonzero_cols, dtype = int)].set(_theta)
+        inv = jnp.block([
+            [_inv, jnp.zeros((_nonzero_cols, _zero_cols))], 
+            [jnp.zeros((_zero_cols, _nonzero_cols)), (1/regularization) * jnp.eye(_zero_cols)]
+        ])
+        return theta[_inverse_permute], inv[:, _inverse_permute][_inverse_permute, :]
+
+    # cannot vmap because nonzero_cols cannot be converted to a matrix form
+    _theta_and_inv = jax.tree_util.tree_map(
+        lambda perm_iter, inv_perm_iter, cols_iter, target_iter : unconstrained(A, perm_iter, inv_perm_iter, cols_iter, target_iter), 
+        [*permute], [*inverse_permute], [*nonzero_cols], [*target]
+    )
+    x_hat, inverses = jnp.stack([_theta for _theta, _ in _theta_and_inv]), jnp.stack([_inv for _, _inv in _theta_and_inv]) # unconstrained solution and pseudo inverses.
+
+    # calculate the constraints part 
+    # g_x.T @ (g_x @ (A.T @ A)-1 g_x.T)-1 @ (g - g_x @ x + g_x @ x_hat)
+    lam_star = jnp.linalg.solve(
+        jnp.sum(jax.vmap(lambda _g_x, _inv : _g_x @ _inv @ _g_x.T)(jnp.stack(jnp.array_split(g_x, nx, axis = 1)), inverses), axis = 0), 
+        g - g_x @ theta_guess + g_x @ x_hat.flatten()
+    )
+
+    x_star = jax.vmap(lambda _x_hat, _inv, _con : _x_hat - _inv @ _con)(x_hat, inverses, jnp.stack(jnp.array_split(g_x.T @ lam_star, nx)))
+    return x_star, (lam_star, inverses)
 
 def _optimal_parameters_fwd(p, target, features, temperature, permute, inverse_permute, nonzero_cols, regularization):
-    theta, (u, s, vh) = _optimal_parameters(p, target, features, temperature, permute, inverse_permute, nonzero_cols, regularization)
-    return (theta, (u, s, vh)), (p, theta, (u, s, vh), target, features, temperature, permute)
+    theta, (lam_star, inv) = _optimal_parameters(p, target, features, temperature, permute, inverse_permute, nonzero_cols, regularization)
+    return (theta, (lam_star, inv)), (p, theta, lam_star, inv, target, features, temperature, permute, inverse_permute)
 
 def _optimal_parameters_bwd(nonzero_cols, regularization, res, g_dot):
     # save inverse during the forward pass and reuse in reverse pass
-    p, theta, (u, s, vh), target, features, temperature, permute = res
+    p, theta, lam_star, inverses, target, features, temperature, permute, inverse_permute = res
+    theta_dot, _ = g_dot
+    nx, _ = theta.shape
 
-    def _g(p):
-        A = _data_matrix(p, features, temperature)
-        A = A[:, permute]
-        _theta = theta[permute]
+    g_x = jax.jacobian(lambda _theta : equality_constraints(p, _theta, nx))(theta.flatten()) # theta primals can be zero since constraints are linear wrt theta
 
-        m, _ = A.shape
-        _A = jax.lax.slice(A, (0, 0), (m, nonzero_cols))
-        _theta = jax.lax.slice(_theta, (0, ), (nonzero_cols, ))
-        return _A.T @ target - _A.T @ (_A @ _theta) - regularization*_theta, _A
+    def L_xp(cotangent : List[jnp.ndarray]):
 
-    _, f_vjp, _A = jax.vjp(_g, p, has_aux = True)
-    cotangent = vh.T @ (jnp.diag(1/s) @ (u.T @ jax.lax.slice(g_dot[0][permute], (0, ), (nonzero_cols, ))))
-    # cotangent = jnp.linalg.solve(_A.T @ _A + regularization * jnp.eye(nonzero_cols), jax.lax.slice(g_dot[0][permute], (0, ), (nonzero_cols, )))
-    return f_vjp(cotangent)[0], None, None, None, None, None
+        def L_x(p, _theta, _permute, _inverse_permute, _nonzero_cols, _target):
+            # gradient of lagrangian wrt to theta
+            A = _data_matrix(p, features, temperature)
+            A = A[:, _permute]
+            
+            m, n = A.shape
+            _A = jax.lax.slice(A, (0, 0), (m, _nonzero_cols))
+            _A = jnp.block([_A, jnp.zeros((m, n - _nonzero_cols))])[:, _inverse_permute]
+            return - _A.T @ _target + _A.T @ (_A @ _theta)
+        
+        Lxp_vjp = jax.tree_util.tree_map(
+            lambda _theta, _perm, _inv_perm, _nzero, _tar : jax.vjp(lambda _p : L_x(_p, _theta, _perm, _inv_perm, _nzero, _tar), p)[-1], 
+            [*theta], [*permute], [*inverse_permute], [*nonzero_cols], [*target]
+        )
+
+        lxp = jax.tree_util.tree_reduce(
+            operator.add,
+            [_Lxp_vjp(_cotan)[0] for _Lxp_vjp, _cotan in zip(Lxp_vjp, cotangent)]
+        )
+
+        _, gxp_vjp = jax.vjp(lambda p : jax.vjp(lambda _theta : equality_constraints(p, _theta, nx), theta.flatten())[-1](lam_star)[0], p)
+        return lxp + gxp_vjp(jnp.concatenate(cotangent))[0]
+
+    _, gp_vjp = jax.vjp(lambda _p : equality_constraints(_p, theta.flatten(), nx), p)
+    
+    cotangent_Lxp = jax.vmap(lambda _g_dot, _inv : _inv @ _g_dot)(theta_dot, inverses) # left inverse == right inverse for a symmetric matrix
+    dx_dp = - L_xp([*cotangent_Lxp])
+
+    cotangent_gp = jnp.linalg.solve(
+        jnp.sum(jax.vmap(lambda _g_x, _inv : _g_x @ _inv @ _g_x.T)(jnp.stack(jnp.array_split(g_x, nx, axis = 1)), inverses), axis = 0), 
+        g_x @ cotangent_Lxp.flatten()
+    )
+    
+    dx_dp -= gp_vjp(cotangent_gp)[0]
+
+    cotangent_Lxp = cotangent_gp @ jnp.column_stack(jax.vmap(lambda _g_x, _inv : _g_x @ _inv)(jnp.stack(jnp.array_split(g_x, nx, axis = 1)), inverses))
+    dx_dp += L_xp(jnp.array_split(cotangent_Lxp, nx))
+
+    return dx_dp, None, None, None, None, None
 
 
 _optimal_parameters.defvjp(_optimal_parameters_fwd, _optimal_parameters_bwd)
@@ -126,11 +198,11 @@ def poly(x, t):
         x[1]**2, x[1]*x[2], x[1]*x[3],
         x[2]**2, x[2]*x[3],
         x[3]**2,
-        x[0]**3, x[0]**2*x[1], x[0]**2*x[2], x[0]**2*x[3],
-        x[1]**3, x[1]**2*x[0], x[1]**2*x[2], x[1]**2*x[3],
-        x[2]**3, x[2]**2*x[0], x[2]**2*x[1], x[2]**2*x[3],
-        x[3]**3, x[3]**2*x[0], x[3]**2*x[1], x[3]**2*x[2],
-        x[0]*x[1]*x[2], x[0]*x[1]*x[3], x[1]*x[2]*x[3], x[0]*x[2]*x[3]
+        # x[0]**3, x[0]**2*x[1], x[0]**2*x[2], x[0]**2*x[3],
+        # x[1]**3, x[1]**2*x[0], x[1]**2*x[2], x[1]**2*x[3],
+        # x[2]**3, x[2]**2*x[0], x[2]**2*x[1], x[2]**2*x[3],
+        # x[3]**3, x[3]**2*x[0], x[3]**2*x[1], x[3]**2*x[2],
+        # x[0]*x[1]*x[2], x[0]*x[1]*x[3], x[1]*x[2]*x[3], x[0]*x[2]*x[3]
     ])
 
 def poly_interp(z, t, x):
@@ -140,11 +212,11 @@ def poly_interp(z, t, x):
         x[1](t)**2, x[1](t)*x[2](t), x[1](t)*x[3](t),
         x[2](t)**2, x[2](t)*x[3](t),
         x[3](t)**2,
-        x[0](t)**3, x[0](t)**2*x[1](t), x[0](t)**2*x[2](t), x[0](t)**2*x[3](t),
-        x[1](t)**3, x[1](t)**2*x[0](t), x[1](t)**2*x[2](t), x[1](t)**2*x[3](t),
-        x[2](t)**3, x[2](t)**2*x[0](t), x[2](t)**2*x[1](t), x[2](t)**2*x[3](t),
-        x[3](t)**3, x[3](t)**2*x[0](t), x[3](t)**2*x[1](t), x[3](t)**2*x[2](t),
-        x[0](t)*x[1](t)*x[2](t), x[0](t)*x[1](t)*x[3](t), x[1](t)*x[2](t)*x[3](t), x[0](t)*x[2](t)*x[3](t)
+        # x[0](t)**3, x[0](t)**2*x[1](t), x[0](t)**2*x[2](t), x[0](t)**2*x[3](t),
+        # x[1](t)**3, x[1](t)**2*x[0](t), x[1](t)**2*x[2](t), x[1](t)**2*x[3](t),
+        # x[2](t)**3, x[2](t)**2*x[0](t), x[2](t)**2*x[1](t), x[2](t)**2*x[3](t),
+        # x[3](t)**3, x[3](t)**2*x[0](t), x[3](t)**2*x[1](t), x[3](t)**2*x[2](t),
+        # x[0](t)*x[1](t)*x[2](t), x[0](t)*x[1](t)*x[3](t), x[1](t)*x[2](t)*x[3](t), x[0](t)*x[2](t)*x[3](t)
     ])
 
 def _foo(x, t, T, theta, p):
@@ -160,25 +232,24 @@ def simple_objective(p, states, features, derivatives, permute, inverse_permute,
     # cannot vmap because nonzero_cols cannot be converted to a matrix using vmap
     # cannot scan over the list nonzero_cols either.
     # unrolling the loop is the only option
-    """
-    theta = jnp.vstack([
-        _optimal_parameters(p, deri_iter, features, temperature, perm_iter, inv_perm_iter, cols_iter, regularization)[0]
-        for deri_iter, perm_iter, inv_perm_iter, cols_iter in zip(jnp.vstack(derivatives).T, permute, inverse_permute, nonzero_cols)
-    ]) """
+
     # TODO get theta from an optimization problem. Equality constraint convex problem
+    """
     theta = jnp.vstack(
         jax.tree_util.tree_map(
             lambda deri_iter, perm_iter, inv_perm_iter, cols_iter : _optimal_parameters(p, deri_iter, features, temperature, perm_iter, inv_perm_iter, cols_iter, regularization)[0], 
             [*jnp.vstack(derivatives).T], [*permute], [*inverse_permute], [*nonzero_cols]
         )
-    )
+    )"""
+
+    theta, _ = _optimal_parameters(p, jnp.vstack(derivatives).T, features, temperature, permute, inverse_permute, nonzero_cols, regularization)
 
     solution = jax.vmap(lambda xi, ti : odeint(_foo, xi, time_span, ti, theta, p))(xinit, temperature)
     _loss = jax.vmap(lambda pred, meas : jnp.mean((pred - meas)**2))(solution, states)
     return jnp.mean(_loss), theta
 
 len_features = len(poly(xinit[0], 0.))
-nonzero_cols = jnp.array([2, 2, 1, 1], dtype = int) # number of nonzero columns
+nonzero_cols = jnp.array([2, 2, 1, 1], dtype = int).tolist() # number of nonzero columns
 permute = jnp.array([0, 1, 2, 3, 0, 1, 2, 3, 2, 0, 1, 3, 2, 0, 1, 3], dtype = int).reshape(nx, -1) # big indexes followed by small indexes
 inverse_permute = jnp.array([0, 1, 2, 3, 0, 1, 2, 3, 1, 2, 0, 3, 1, 2, 0, 3], dtype = int).reshape(nx, -1) # reverse the permutation
 p_guess = jnp.ones(len_features)
@@ -190,19 +261,32 @@ sindy_features = jax.vmap(jax.vmap(lambda xi : poly(xi, 0.)))(solution)
 # DF-SINDY features
 dfsindy_features = jnp.stack([scipy_odeint(poly_interp, poly(_xi, 0), time_span, args = (_interp, )) - poly(_xi, 0) for _xi, _interp in zip(xinit, interpolations)])
 
-# loss, linear_params = simple_objective(p_guess, solution, features, estimated_derivatives, permute, inverse_permute, nonzero_cols)
-# gradients, aux = jax.grad(simple_objective, has_aux = True)(p_guess, solution, features, estimated_derivatives, permute, inverse_permute, nonzero_cols)
+# loss, linear_params = simple_objective(p_guess, solution, sindy_features, estimated_derivatives, permute, inverse_permute, nonzero_cols)
+# gradients, aux = jax.grad(simple_objective, has_aux = True)(p_guess, solution, sindy_features, estimated_derivatives, permute, inverse_permute, nonzero_cols)
 
-# _simple_obj = jax.jit(lambda p : simple_objective(p, solution, features, estimated_derivatives, permute, inverse_permute, nonzero_cols)[0])
+# _simple_obj = jax.jit(lambda p : simple_objective(p, solution, sindy_features, estimated_derivatives, permute, inverse_permute, nonzero_cols)[0])
 # _simple_jac = jax.jit(jax.grad(_simple_obj))
 # _simple_hess = jax.jit(jax.jacrev(_simple_jac))
 
+"""
+### checking gradients with finite difference
+eps = 1e-5 
+fd0 = (_simple_obj(p_guess + jnp.array([eps, 0., 0., 0.])) - loss) / eps
+fd1 = (_simple_obj(p_guess + jnp.array([0., eps, 0., 0.])) - loss) / eps
+fd2 = (_simple_obj(p_guess + jnp.array([0., 0., eps, 0.])) - loss) / eps
+fd3 = (_simple_obj(p_guess + jnp.array([0., 0., 0., eps])) - loss) / eps
+
+print("gradients fd", fd0, fd1, fd2, fd3)
+print("gradients autodiff", gradients)
+"""
 
 def outer_objective(p_guess, theta_guess, solution, features, estimated_derivatives, thresholding = 0.1, maxiter = 10):
     # implement sequential threshold least square algorithm
     # TODO previous subproblems can have lower tolerance or less maximum iterations to eliminate the terms. How to choose initial tolerance ?
+    # TODO how to eliminate coefficients that have equality constraints dependant on others coefficients that are kept
+    # eg : constraint x1 + x2 = 0. If x1 has to be kept and x2 is neglected then how to remove x2 if the constraint forces x2 to be -x1
 
-    def get_permutations(theta):
+    def get_permutations(theta) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, List[jnp.ndarray]]:
 
         ind = jnp.arange(len(theta), dtype = int)
         big_ind = jnp.abs(theta) >= thresholding
@@ -232,7 +316,7 @@ def outer_objective(p_guess, theta_guess, solution, features, estimated_derivati
         if iteration > 0 and jnp.allclose(mask, prev_mask) : 
             print("Optimal solution found")
             break
-
+        
         _simple_obj = jax.jit(lambda p : simple_objective(p, solution, features, estimated_derivatives, permute, inverse_permute, nonzero_cols)[0])
         _simple_jac = jax.jit(jax.grad(_simple_obj))
         _simple_hess = jax.jit(jax.jacrev(_simple_jac))
